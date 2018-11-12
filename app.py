52e1dcf5739811e31991ee86382a8e5ed5247fe2
Samuel Davidson <samdamana@gmail.com>
2018-11-11 19:05:08 -0800

    Fixed makeup

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ def loop():
     face_locations = face_recognition.face_locations(image)
 
     if len(face_locations) == 0:
-        print('No face found')
         return
 
     face_landmarks_list = face_recognition.face_landmarks(image, face_locations=face_locations[:1])
@@ -30,28 +29,34 @@ def loop():
         return
     face_location = face_locations[0]
     face_landmarks = face_landmarks_list[0]
-    pil_image = Image.fromarray(image)
+    capture_image = Image.fromarray(image)
+    pil_image = Image.new('RGBA', capture_image.size, color=(255, 255, 255, 255))
     d = ImageDraw.Draw(pil_image, 'RGBA')
 
     # Make the eyebrows into a nightmare
-    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
-    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
-    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    brow_fill = (0, 0, 0, 128)
+    brow_line = (0, 0, 0, 150)
+    d.polygon(face_landmarks['left_eyebrow'], fill=brow_fill)
+    d.polygon(face_landmarks['right_eyebrow'], fill=brow_fill)
+    d.line(face_landmarks['left_eyebrow'], fill=brow_line, width=5)
+    d.line(face_landmarks['right_eyebrow'], fill=brow_line, width=5)
 
     # Gloss the lips
-    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
-    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
-    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
-    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+    lip_fill = (0, 0, 0, 128)
+    lip_line = (0, 0, 0, 64)
+    d.polygon(face_landmarks['top_lip'], fill=lip_fill)
+    d.polygon(face_landmarks['bottom_lip'], fill=lip_fill)
+    d.line(face_landmarks['top_lip'], fill=lip_line, width=8)
+    d.line(face_landmarks['bottom_lip'], fill=lip_line, width=8)
 
-    # Sparkle the eyes
-    d.polygon(face_landmarks['left_eye'], fill=(255, 255, 255, 30))
-    d.polygon(face_landmarks['right_eye'], fill=(255, 255, 255, 30))
-
-    # Apply some eyeliner
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    # # Sparkle the eyes
+    # eye_fill = (255, 255, 255, 30)
+    # d.polygon(face_landmarks['left_eye'], fill=eye_fill)
+    # d.polygon(face_landmarks['right_eye'], fill=eye_fill)
+    #
+    # # Apply some eyeliner
+    # d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    # d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
 
     face = pil_image.crop((face_location[3], face_location[0], face_location[1], face_location[2]))
     face.save(output_image)
@@ -75,7 +80,7 @@ def set_drawn_image(path):
     retry = True
     while retry:
         try:
-            subprocess.run(['fbi', '-T', '2', '-a', output_image], check=True)
+            subprocess.run(['fbi', '-T', '2', '-a', output_image], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
             retry = False
         except subprocess.CalledProcessError as e:
             error(e)
@@ -94,6 +99,6 @@ def error(e):
         sys.exit()
 
 
+print('Running main loop...')
 while True:
-    print('Running main loop...')
     loop()
