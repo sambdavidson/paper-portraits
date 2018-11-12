14540f3fe48169ba06bab1f7681f8ed517fb6407
Samuel Davidson <samdamana@gmail.com>
2018-11-11 16:54:17 -0800

    Cleaned up code

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ from PIL import Image
 from shutil import copyfile
 import face_recognition
 import subprocess
+import sys
 
 input_image = "sample/capture.jpg"
-working_image = "sample/working.jpg"
 output_image = "sample/face.jpg"
 
-while True:
-    # Copy the input image to working image because input is being modified constantly
-    copyfile(input_image, working_image)
+capture_path = "sample/capture.jpg"
 
-    real_image = Image.open(working_image)
-    real_image.show()
-    numpy_image = face_recognition.load_image_file(working_image)
+ERROR_LIMIT = 10
+error_count = 0
+
+
+def loop():
+    reset_error_count()
+    capture_image_to_path(capture_path)
+    real_image = Image.open(input_image)
+    numpy_image = face_recognition.load_image_file(input_image)
     face_locations = face_recognition.face_locations(numpy_image)
     if len(face_locations) > 0:
         up = face_locations[0][0]
@@ -26,3 +30,27 @@ while True:
         subprocess.run(['fbi', '-T', '2', '-a', output_image])
     else:
         print("No faces found")
+
+# Captures image from webcam and saves to the specified path
+def capture_image_to_path(path):
+    try:
+        subprocess.run(['fswebcam', '-r', '640x480', '--no-banner', '--save', path], check=True)
+    except subprocess.CalledProcessError as e:
+        error(e)
+
+
+def reset_error_count():
+    global error_count
+    error_count = 0
+
+
+def error(e):
+    global error_count
+    print('Error: {}'.format(e))
+    error_count += 1
+    if error_count == ERROR_LIMIT:
+        sys.exit()
+
+
+while True:
+    loop()
--- setup.md
+++ setup.md
@@ -6,7 +6,30 @@ Github: https://github.com/ageitgey/face_recognition
 
 Raspberry Pi Installation: https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
 
+Also run:
+```bash
+sudo apt-get install libatlas-base-dev
+```
+
 Just follow those and then move on.
+
+## Capture webcam image (command line tool)
+
+Installation 
+```bash
+sudo apt-get install fswebcam
+```
+
+Run
+
+```bash
+fswebcam -r 1280x720 --no-banner capture.jpg
+```
+
+Run continuously in background
+```bash
+fswebcam -r 1280x720 --no-banner --loop 1 --background --pid sample/fswebcam_pid.txt sample/capture.jpg
+```
 
 ## Display Images FBI
 
