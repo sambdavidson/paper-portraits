f8d637d0176c5b482d6e4042bff77ca132486449
Samuel Davidson <samdamana@gmail.com>
2018-12-05 21:45:06 -0800

    Added actual exit/shutdown code.

--- shutdown_button.py
+++ shutdown_button.py
@@ -1,6 +1,7 @@
 # Handles the shutdown button
 from gpiozero import Button
 from threading import Timer
+from subprocess import call
 
 GPIO_PIN = 5
 REQUIRED_CLICKS = 5
@@ -41,12 +42,14 @@ def shutdown():
     global pre_shutdown_function, __shutting_down
     __shutting_down = True
 
-    print("SHUTTING DOWN")
     if pre_shutdown_function is not None:
         if callable(pre_shutdown_function):
             pre_shutdown_function()
         else:
             print("WARNING: pre-shutdown function not callable")
+
+    # call("sudo shutdown -h now", shell=True) # UNCOMMENT WHEN ACTUALLY IN PHOTO FRAME
+    quit() # Used instead of shutdown for testing.
 
 
 button = Button(GPIO_PIN)
