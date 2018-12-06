08d7428c73ec061f4a110c17c33446a999e1e1c9
Samuel Davidson <samdamana@gmail.com>
2018-12-05 21:40:34 -0800

    Added shutting down flag and updated pic.

--- shutdown_button.py
+++ shutdown_button.py
@@ -11,11 +11,19 @@ timeout_timer = None
 # Function to run before shutdown. Called if defined by user.
 pre_shutdown_function = None
 
+# Tracks if we are shutting down. Don't want to call shutdown callbacks a bunch
+__shutting_down = False
+
 
 def click_with_timeout():
-    global active_clicks, timeout_timer
+    global active_clicks, timeout_timer, __shutting_down
+
+    if __shutting_down:
+        return
+
     if timeout_timer is not None:
         timeout_timer.cancel()
+
     timeout_timer = Timer(TIMEOUT, reset)
     active_clicks += 1
     if active_clicks >= REQUIRED_CLICKS:
@@ -30,14 +38,15 @@ def reset():
 
 def shutdown():
     """Shuts down the Pi, called once enough clicks happened."""
-    global pre_shutdown_function
+    global pre_shutdown_function, __shutting_down
+    __shutting_down = True
+
+    print("SHUTTING DOWN")
     if pre_shutdown_function is not None:
         if callable(pre_shutdown_function):
             pre_shutdown_function()
         else:
             print("WARNING: pre-shutdown function not callable")
-
-    print("SHUTTING DOWN")
 
 
 button = Button(GPIO_PIN)
