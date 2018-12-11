8016f9882534666492cc2b29963523a85c74b114
Samuel Davidson <samdamana@gmail.com>
2018-12-11 06:47:53 +0000

    First parts of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
 echo "Setting up Paper Portraits."
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root."
   exit
 fi
 
-cat "Installing PIP3..."
+cat "Installing Python3 Pillow"
+apt-get install python3-pillow -qq
+
+cat "Installing Python3 gpiozero"
+apt-get install python3-gpiozero -qq
+
+cat "Installing Python3 Facial Recognition"
+
